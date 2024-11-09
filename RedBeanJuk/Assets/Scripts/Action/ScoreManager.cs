304f29314bc697ff7dfd612b5395a0f789c0fbfc
txmxtx <dxxeon@ewhain.net>
2024-11-09 13:31:57 +0900

    재료이동, 성패여부

--- RedBeanJuk/Assets/Scripts/Action/ScoreManager.cs
+++ RedBeanJuk/Assets/Scripts/Action/ScoreManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
     public Cooking cooking;
     public moveIngredients moveIng;
@@ -24,14 +24,5 @@ public class GameManager : MonoBehaviour
     }
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- RedBeanJuk/Assets/Scripts/Action/moveIngredients.cs
+++ RedBeanJuk/Assets/Scripts/Action/moveIngredients.cs
@@ -1,17 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class moveIngredients : MonoBehaviour, IDragHandler, IEndDragHandler
+public class moveIngredients : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler 
 {
+    private void Start() {
+        Vector3 startPosition = transform.position;
+    }
+    private RectTransform rectTransform;
     public List<string> addedIngredients = new List<string>();
     public float minDis= 0.1f;
     public GameObject pot;
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+    }
+
     public void OnDrag(PointerEventData eventData) {
-        transform.position = eventData.position;
-        Debug.Log("dragging");
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    private Vector3 = startPosition;
+    private void Start() {
+        startPosition = transform.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+        string currentIngName = gameObject.name;
+        if(Vector3.Distance(pot.transform.position, transform.position) < minDis) {
+            AddIngredient(currentIngName);
+            transform.position = startPosition;
+            // Destroy(gameObject);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("OnPointerDown");
     }
 
     public void AddIngredient(string ingredientName) {
@@ -19,26 +49,29 @@ public class moveIngredients : MonoBehaviour, IDragHandler, IEndDragHandler
         Debug.Log("아이템 추가");
     }
 
-    public void OnEndDrag(PointerEventData eventData){
-        string currentIngName = gameObject.name;
+    // IDragHandler, IEndDragHandler
+    // public List<string> addedIngredients = new List<string>();
+    // public float minDis= 0.1f;
+    // public GameObject pot;
+    // public void OnDrag(PointerEventData eventData) {
+    //     transform.position = eventData.position;
+    //     Debug.Log("dragging");
+    // }
 
-        Debug.Log("end drag");
+    // public void AddIngredient(string ingredientName) {
+    //     addedIngredients.Add(ingredientName);
+    //     Debug.Log("아이템 추가");
+    // }
 
-        if(Vector3.Distance(pot.transform.position, transform.position) < minDis) {
-            AddIngredient(currentIngName);
-            Destroy(gameObject);
-        }
-    }
+    // public void OnEndDrag(PointerEventData eventData){
+    //     string currentIngName = gameObject.name;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //     Debug.Log("end drag");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //     if(Vector3.Distance(pot.transform.position, transform.position) < minDis) {
+    //         AddIngredient(currentIngName);
+    //         Destroy(gameObject);
+    //     }
+    // }
+
 }
