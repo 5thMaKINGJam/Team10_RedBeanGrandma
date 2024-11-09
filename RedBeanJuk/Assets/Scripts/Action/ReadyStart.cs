a10650355d25b3bb426e3e00c421ebc25fe5ba98
txmxtx <dxxeon@ewhain.net>
2024-11-09 20:23:17 +0900

    ready-start

---
+++ RedBeanJuk/Assets/Scripts/Action/ReadyStart.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class ReadyStart : MonoBehaviour
+{
+    public GameObject startimg;
+    public GameObject readyimg;
+    public Material[] mat = new Material[2];
+    private Image background;
+    private void Start()
+    {
+        background = GetComponent<Image>();
+        StartCoroutine(LoadGame());
+    }
+    
+    private IEnumerator LoadGame() {
+        readyimg.SetActive(true);
+        background.material = mat[0];
+
+        yield return new WaitForSeconds(2f);
+        startimg.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        startimg.SetActive(false);
+        readyimg.SetActive(false);
+        background.material = mat[1];
+    }
+}
