63d9bef37d22074605503b0fb6014b2994e8023e
emilyminkyounglee <minkyoungemilylee@ewha.ac.kr>
2024-11-09 17:40:26 +0900

    Animation and buttons

--- RedBeanJuk/Assets/Scripts/Action/ButtonsAndNext.cs
+++ RedBeanJuk/Assets/Scripts/Action/ButtonsAndNext.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsAndNext : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void goTo_1_2_storyScene()
     {
-        
+        SceneManager.LoadScene("_1-2Story");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
     }
 }
