2a825084fb6f248915e3994b21fccc5e446e7941
txmxtx <dxxeon@ewhain.net>
2024-11-10 10:57:08 +0900

    초반 자잘한 씬들 정리..

--- RedBeanJuk/Assets/ScreenLoader.cs
+++ RedBeanJuk/Assets/ScreenLoader.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class ScreenLoader : MonoBehaviour
+public class ScreenLoader : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public void LoadScene() {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Tutorial_ing");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        transform.localScale = Vector3.one;
     }
 }
--- RedBeanJuk/Assets/Scripts/Action/TypingEffect.cs
+++ RedBeanJuk/Assets/Scripts/Action/TypingEffect.cs
@@ -1,12 +1,14 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TypingEffect : MonoBehaviour
+public class TypingEffect : MonoBehaviour, IPointerDownHandler
 {
     public Text uiText;
     string fullText = "아주 먼 옛날 깊은 산 속에\n팥죽을 아주 잘 끓이는 할머니가 살고 있었습니다.\n\n어느 봄날 호랑이가 내려와\n할머니를 잡아먹으려고 하였습니다.\n\n할머니는 동짓날 맛있는 팥죽을 끓여줄테니\n기다려달라고 호랑이를 구슬렸습니다.\n\n호랑이는 동짓날 다시 오겠다고 하고\n훌쩍 산속으로 돌아갔습니다.\n\n동짓날 아침이 되자 할머니는 호랑이가\n잡아 먹으러 온다는 생각에 울고 있었습니다.\n\n그 모습을 보고 알밤, 거북이, 진흙, 송곳, 절구가 찾아왔고\n맛있는 팥죽을 주면 할머니를 도와주겠다고 합니다.\n\n그럼 팥죽을 끓이러 가볼까요?  ";
-    public float typingSpeed = 0.1f;
+    public float typingSpeed = 0.05f;
 
     private void Start()
     {
@@ -20,6 +22,13 @@ public class TypingEffect : MonoBehaviour
         {
             uiText.text += fullText[i];
             yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        typingSpeed -= 0.01f;
+        if (typingSpeed < 0.01f) {
+            typingSpeed = 0.001f;
         }
     }
 }
