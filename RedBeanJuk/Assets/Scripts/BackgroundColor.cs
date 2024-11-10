3c335d9d90c77d164446b406f1ba04f5bf4c7aa8
emilyminkyounglee <minkyoungemilylee@ewha.ac.kr>
2024-11-10 09:16:20 +0900

    게임씬 배경화면 색상 추가

--- RedBeanJuk/Assets/Scripts/BackgroundColor.cs
+++ RedBeanJuk/Assets/Scripts/BackgroundColor.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class BackgroundColor : MonoBehaviour
 {
+    [SerializeField] private GameObject[] stageColor;
+
+    private int index = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject gO in stageColor)
+        {
+            gO.SetActive(false);
+        }
+        stageColor[0].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        index = GameManager.Instance.GetStage();
+        while (index >= 2)
+        {
+            stageColor[index-1].SetActive(true);
+            stageColor[index-2].SetActive(false);
+        }
     }
 }
